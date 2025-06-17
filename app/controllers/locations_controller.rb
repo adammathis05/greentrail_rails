9d43c8cee7632a7f4edbbc969f69ff147583c048
Adam Mathis <adam.mathis05@gmail.com>
2025-06-17 14:39:13 -0500

    adjusted locations_controller and location index view file

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,7 @@
 class LocationsController < ApplicationController
 
     def index
-        @locations = ["Chacala, Mexico", "Monterey, CA", "Siem Reap, Cambodia", "Lisbon, Portugal"]
+        @locations = Location.all
     end
 
 end
