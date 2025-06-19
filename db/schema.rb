59b30eb815cd084753a08611a6f99d0c87100cdf
Adam Mathis <adam.mathis05@gmail.com>
2025-06-19 14:14:28 -0500

    sets up FactoryBot in Spec and adds type column to locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_19_181200) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_19_191342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_19_181200) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "provider_tags", force: :cascade do |t|
